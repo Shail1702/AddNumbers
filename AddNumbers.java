fefd273dc3a587a3912edb25b47608c2148cf21a
Shail <shail.singh@gmail.com>
2022-08-21 00:09:32 +0530

    Added Add Numbers java Program

---
+++ AddNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // Import the Scanner class
+
+class AddNumbers {
+  public static void main(String[] args) {
+    int x, y, sum;
+    x = 5; // Read user input
+
+    y = 6; // Read user input
+
+    sum = x + y;  // Calculate the sum of x + y
+    System.out.println("Sum is: " + sum); // Print the sum
+  }
+} 
